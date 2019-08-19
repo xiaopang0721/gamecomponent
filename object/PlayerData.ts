7620ca52ebca3fabe2539c217382c12427e5cd9f
happy <happy>
2019-08-19 14:06:47 +0800

    玩家金币下标

--- object/PlayerData.ts
+++ object/PlayerData.ts
@@ -99,6 +99,7 @@ module gamecomponent.object {
 				ness++;
 			}
 			if (isNew || mask.GetBit(PlayerData.PLAYERDATA_INT_MONEY)) {
+				//下标值获取到后除以100，得到带两位小数点的金币数，GetMoney下标值只有这里能用
 				this._playerInfo.money = EnumToString.getPointBackNum(this.GetMoney() / 100, 2)
 				ness++;
 			}

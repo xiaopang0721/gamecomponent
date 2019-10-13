1b306879707c2b111722e33913cb896ddcd9f639
atubo <atubo@h.com>
2019-10-13 15:06:18 +0800

    二维码

--- component/QRCodeSprite.ts
+++ component/QRCodeSprite.ts
@@ -3,52 +3,77 @@
 */
 module gamecomponent.component {
     export class QRCodeSprite extends laya.display.Sprite {
-        private _url: string;
-        private _imgW: number;
-        private _imgH: number;
-        private _qrcode:any;
-        private _complate:Function;
 
-        constructor(url: string, imgWidth: number, imgHeight: number, complate?:Function) {
+        private static QRCodes = {};
+        static createQRCodeBase64(url, imgWidth: number, imgHeight: number, complate: Function) {
+            let qr = this.QRCodes[url] as QRCodeBase64;
+            if (qr) {
+                if (qr.base64)
+                    complate && complate(qr.base64);
+                else {                    
+                    qr.on(LEvent.COMPLETE, this, ()=>{
+                        complate && complate(qr.base64);
+                        qr.offAll();
+                    });
+                }
+                return;
+            }                
+            qr = new QRCodeBase64(url, imgWidth, imgHeight);
+            qr.on(LEvent.COMPLETE, this, ()=>{
+                complate && complate(qr.base64);
+                qr.offAll();
+            });
+            this.QRCodes[url] = qr;
+        }
+        static deleteQRCodeBase64(url) {
+            let qr = this.QRCodes[url];
+            if (qr instanceof QRCodeBase64) {
+                qr.offAll();
+                qr.clear();
+                delete this.QRCodes[url];
+            }
+        }
+
+        constructor(url: string, imgWidth: number, imgHeight: number, complate?: Function) {
             super();
-            this._url = url;
-            this._imgW = imgWidth;
-            this._imgH = imgHeight;
-            this._complate = complate;
-            this.show();
+            QRCodeSprite.createQRCodeBase64(url, imgWidth, imgHeight, (base64)=>{
+                this.loadImage(base64, 0, 0, imgWidth, imgHeight, Handler.create(this, () => {
+                    complate && complate(url);
+                }));
+            });
         }
 
         destroy(destroyChild?: boolean): void {
-            if (this._qrcode) {
-                this._qrcode.clear();
-                this._qrcode = null;
-            }
-            this._url = null;
-            this._complate = null;
             this.removeSelf();
             Laya.timer.clearAll(this);
             super.destroy(destroyChild);
         }
+    }
 
-        private show(): void {
-            let div: any = Laya.Browser.document.createElement("div");
-            this._qrcode = new Laya.Browser.window.QRCode(div, {
-                width: this._imgW,
-                height: this._imgH
-            });
-            this._qrcode.makeCode(this._url);
-            Laya.timer.frameOnce(1, this, this.onCheckSrc);            
+    class QRCodeBase64 extends EventDispatcher{
+        private _base64: string;
+        get base64(): string {
+            return this._base64;
         }
 
-        private onCheckSrc():void{
-            let src = this._qrcode._oDrawing._elImage.src;
-            if (src) {
-                Laya.timer.clearAll(this);
-                this.loadImage(src, 0, 0, this._imgW, this._imgH, Handler.create(this, ()=>{
-                    this._complate && this._complate(this._url);
-                }));
-                return;
-            }
+        constructor(url, imgW, imgH) {
+            super();
+            let div: any = Laya.Browser.document.createElement("div");
+            let qrcode = new Laya.Browser.window.QRCode(div, {
+                width: imgW,
+                height: imgH
+            });
+            qrcode.makeCode(url);
+            Laya.timer.frameOnce(1, this, () => {
+                this._base64 = qrcode._oDrawing._elImage.src;
+                this.event(LEvent.COMPLETE, this._base64);
+                div = null;
+                qrcode = null;
+            });
+        }
+
+        clear() {
+            Laya.timer.clearAll(this);
         }
     }
 }

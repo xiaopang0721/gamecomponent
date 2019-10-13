732c0d9f1d611c18bfa9fde9aae03ed319a89cfb
xiaoer <xiaoer@example.com>
2019-10-13 20:27:24 +0800

    DisplayU.ts提交

--- component/DisplayU.ts
+++ component/DisplayU.ts
@@ -16,6 +16,7 @@ module gamecomponent.component {
 		private static MASK_KIND_LEFT: number = 5;
 		private static MASK_KIND_RIGHT: number = 6;
 
+		//资源大小
 		private static MASK_HEIGHT: number = 50;
 		private static MASK_WIDHT: number = 50;
 		//遮罩资源
@@ -26,41 +27,54 @@ module gamecomponent.component {
 		private static MASK_IMG_RIGHT: LImage;
 
 		private static MASK_DICT: Object;
+		/**横向滑动 */
+		public static SLIDE_V: number = 1;
+		/**竖向滑动 */
+		public static SLIDE_H: number = 2;
 		private static initMask() {
-			this.MASK_IMG_TOP = new LImage(DatingPath.ui_dating_tongyong + "mask_top.png");
-			this.MASK_IMG_BOTTOM = new LImage(DatingPath.ui_dating_tongyong + "mask_bottom.png");
-			this.MASK_IMG_MIDDLE = new LImage(DatingPath.ui_dating_tongyong + "mask_middle.png");
-			this.MASK_IMG_LEFT = new LImage(DatingPath.ui_dating_tongyong + "mask_left.png");
-			this.MASK_IMG_RIGHT = new LImage(DatingPath.ui_dating_tongyong + "mask_right.png");
+			this.MASK_IMG_TOP = new LImage("dating_ui/tongyong/mask_top.png");
+			this.MASK_IMG_BOTTOM = new LImage("dating_ui/tongyong/mask_bottom.png");
+			this.MASK_IMG_MIDDLE = new LImage("dating_ui/tongyong/mask_middle.png");
+			this.MASK_IMG_LEFT = new LImage("dating_ui/tongyong/mask_left.png");
+			this.MASK_IMG_RIGHT = new LImage("dating_ui/tongyong/mask_right.png");
 			this.MASK_DICT = new Object();
 		}
 
-		public static onScrollChange(target: any, type: number = this.MASK_TYPE_NORMAL): void {
+
+		public static onScrollChange(target: any, type: number = this.MASK_TYPE_NORMAL, slideType: number = DisplayU.SLIDE_H): void {
 			if (!target) return;
 			if (!this.MASK_DICT) this.initMask();
 			if (type == this.MASK_TYPE_NULL) {
-				this.drawMaskByType(target, this.MASK_TYPE_NULL); return;
+				this.drawMaskByType(target, this.MASK_TYPE_NULL, slideType); return;
 			}
 			let scroll: laya.ui.ScrollBar;
 			if (target instanceof Laya.List) scroll = target.scrollBar;
 			else if (target instanceof Laya.Panel) scroll = target.vScrollBar;
-			if (!scroll || !scroll.isVertical) return;
+			if (!scroll) return;
 			if (scroll.min == scroll.max) {
 				if (type == this.MASK_TYPE_RESET) {
-					this.drawMaskByType(target, this.MASK_KIND_ALL);
+					this.drawMaskByType(target, this.MASK_KIND_ALL, slideType);
 					return;
 				}
 			}
 			if (scroll.value <= scroll.min) {
-				this.drawMaskByType(target, this.MASK_KIND_TOP);
+				if (slideType == DisplayU.SLIDE_H) {
+					this.drawMaskByType(target, this.MASK_KIND_TOP, slideType);
+				} else if (slideType == DisplayU.SLIDE_V) {
+					this.drawMaskByType(target, this.MASK_KIND_LEFT, slideType);
+				}
 			} else if (scroll.value >= scroll.max) {
-				this.drawMaskByType(target, this.MASK_KIND_BOTTOM);
-			} else this.drawMaskByType(target, this.MASK_KIND_MIDDLE)
+				if (slideType == DisplayU.SLIDE_H) {
+					this.drawMaskByType(target, this.MASK_KIND_BOTTOM, slideType);
+				} else if (slideType == DisplayU.SLIDE_V) {
+					this.drawMaskByType(target, this.MASK_KIND_RIGHT, slideType);
+				}
+			} else this.drawMaskByType(target, this.MASK_KIND_MIDDLE, slideType)
 		}
 
-		private static drawMaskByType(target: any, kind: number = this.MASK_KIND_ALL): void {
+		private static drawMaskByType(target: any, kind: number = this.MASK_KIND_ALL, slideType: number = DisplayU.SLIDE_H): void {
 			if (!target) return;
-			let key: string = target.$_GID.tostring();
+			let key: string = target.$_GID.toString();
 			if (kind == this.MASK_KIND_NULL) {
 				if (target.parent.mask) target.parent.mask = null;
 				if (this.MASK_DICT[key]) delete this.MASK_DICT[key];
@@ -70,12 +84,13 @@ module gamecomponent.component {
 			let mask: Sprite;
 			if (!this.MASK_DICT[key]) {
 				mask = new Sprite();
-				mask.pos(target.x, target.y);
+				mask.pos(0, 0);
 				mask.size(target.width, target.height);
-				target.parent.mask = mask;
 				this.MASK_DICT[key] = { mask: mask };
 			} else mask = this.MASK_DICT[key].mask;
-			let height: number = this.MASK_DICT[key];
+			if(!target.mask) target.mask = mask;
+			let height: number = DisplayU.MASK_HEIGHT;
+			let width: number = DisplayU.MASK_WIDHT;
 			let graphics: Graphics = mask.graphics;
 			graphics.clear();
 			switch (kind) {
@@ -91,9 +106,29 @@ module gamecomponent.component {
 					break
 				case this.MASK_KIND_MIDDLE:
 					//滚动到位于中间，上下都要遮罩虚化图
-					graphics.fillTexture(this.MASK_IMG_TOP.source, 0, 0, mask.width, height, "repeat-x");
-					graphics.fillTexture(this.MASK_IMG_MIDDLE.source, 0, height, mask.width, mask.height - 2 * height, "repeat");
-					graphics.fillTexture(this.MASK_IMG_BOTTOM.source, 0, mask.height - height, mask.width, height, "repeat-x");
+					if (slideType == DisplayU.SLIDE_H) {
+						graphics.fillTexture(this.MASK_IMG_TOP.source, 0, 0, mask.width, height, "repeat-x");
+						graphics.fillTexture(this.MASK_IMG_MIDDLE.source, 0, height, mask.width, mask.height - 2 * height, "repeat");
+						graphics.fillTexture(this.MASK_IMG_BOTTOM.source, 0, mask.height - height, mask.width, height, "repeat-x");
+					} else if (slideType == DisplayU.SLIDE_V) {
+						graphics.fillTexture(this.MASK_IMG_LEFT.source, 0, 0, width, mask.height, "repeat-y");
+						graphics.fillTexture(this.MASK_IMG_MIDDLE.source, width, 0, mask.width - 2 * width, mask.height, "repeat");
+						graphics.fillTexture(this.MASK_IMG_RIGHT.source, mask.width - width, 0, width, mask.height, "repeat-y");
+					}
+					break
+				case this.MASK_KIND_LEFT:
+					//滚动到位于左侧，中间右侧都要遮罩虚化图
+					graphics.fillTexture(this.MASK_IMG_MIDDLE.source, 0, 0, mask.width - width, mask.height, "repeat");
+					graphics.fillTexture(this.MASK_IMG_RIGHT.source, mask.width - width, 0, width, mask.height, "repeat-y");
+					break
+				case this.MASK_KIND_RIGHT:
+					//滚动到位于右侧，中间左侧都要遮罩虚化图
+					graphics.fillTexture(this.MASK_IMG_MIDDLE.source, width, 0, mask.width, mask.height, "repeat");
+					graphics.fillTexture(this.MASK_IMG_LEFT.source, 0, 0, width, mask.height, "repeat-y");
+					break
+				case this.MASK_KIND_ALL:
+					//全不虚化
+					graphics.fillTexture(this.MASK_IMG_MIDDLE.source, 0, 0, mask.width, mask.height, "repeat");
 					break
 			}
 		}

31d97c42e9bdb0059663284393e8d7a9978bcc4f
happy <happy>
2019-10-27 16:34:30 +0800

    为了更新先还原

--- managers/SceneObjectMgr.ts
+++ managers/SceneObjectMgr.ts
@@ -128,15 +128,9 @@ module gamecomponent.managers {
                         this.ForEachObject((obj) => {
 
                         })
-                        Laya.timer.frameOnce(1, this, ()=>{
-                            this.event(SceneObjectMgr.EVENT_LOAD_MAP, this._mapAssetInfo);
-                        })
+                        this.event(SceneObjectMgr.EVENT_LOAD_MAP, this._mapAssetInfo);
                     }
-                    JsLoader.ins.startLoad(newMapid, Handler.create(this, (asserts) => {
-                        this._game.uiRoot.showLoadProgress("资源加载中...", Handler.create(this, () => {
-                            this._mapAssetInfo.load(newMapid, this._story.mapUrl, this._story.maplv);
-                        }), asserts);
-                    }));
+                    this._mapAssetInfo.load(newMapid, this._story.mapUrl, this._story.maplv);
                 }
             }
         }

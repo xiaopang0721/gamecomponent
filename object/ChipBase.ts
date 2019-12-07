aed4d642d5b6684e2facd8514f9192d94741edf3
xiaoer <xiaoer@example.com>
2019-12-07 18:58:27 +0800

    扔筹码动画修改

--- object/ChipBase.ts
+++ object/ChipBase.ts
@@ -11,20 +11,29 @@ module gamecomponent.object {
 		* 扔筹码
 		*/
 		public sendChip() {
-			if(this.isCanClear) return;//如果可以清理，则不接受任何缓动回调
-			if(!this.pos) return;
+			if (this.isCanClear) return;//如果可以清理，则不接受任何缓动回调
+			if (!this.pos) return;
 			this.isUIShow = true;
 			Laya.Tween.clearAll(this.pos);
+			Laya.Tween.clearAll(this);
+			this.scaleX = 1.4;
+			this.scaleY = 1.4;
+			let rotateAngle = this.rotateAngle + MathU.randomRange(0, 15);
 			Laya.Tween.to(this.pos, { x: this.targe_pos.x, y: this.targe_pos.y }, 500, Laya.Ease.circInOut, Handler.create(this, () => {
 				this.isUIShow = false;
 				this.isFinalPos = true;
+				Laya.Tween.to(this, { scaleX: 1.4, scaleY: 1.4 }, 300, Laya.Ease.circInOut)
 			}));
+			Laya.Tween.to(this, { scaleX: 1.55, scaleY: 1.55 }, 500, Laya.Ease.circInOut)
+			Laya.timer.once(300, this, () => {
+				Laya.Tween.to(this, { rotateAngle: rotateAngle }, 500, Laya.Ease.circInOut)
+			})
 		}
 
 		/**
 		 * 筹码飞到指定位置
 		 */
-		public comebackChip(){
+		public comebackChip() {
 			this.isUIShow = true;
 		}
 	}

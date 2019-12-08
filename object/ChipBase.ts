8426633a547485fdab090f578dba842ff501d23c
happy <happy>
2019-12-09 00:22:11 +0800

    筹码

--- object/ChipBase.ts
+++ object/ChipBase.ts
@@ -16,15 +16,13 @@ module gamecomponent.object {
 			this.isUIShow = true;
 			Laya.Tween.clearAll(this.pos);
 			Laya.Tween.clearAll(this);
-			this.scaleX = 1.4;
-			this.scaleY = 1.4;
 			let rotateAngle = this.rotateAngle + 5;
 			Laya.Tween.to(this.pos, { x: this.targe_pos.x, y: this.targe_pos.y }, 500, Laya.Ease.circInOut, Handler.create(this, () => {
 				this.isUIShow = false;
 				this.isFinalPos = true;
-				Laya.Tween.to(this, { scaleX: 1.4, scaleY: 1.4 }, 300, Laya.Ease.circInOut)
+				Laya.Tween.to(this, { scaleX: 1, scaleY: 1 }, 300, Laya.Ease.circInOut)
 			}));
-			Laya.Tween.to(this, { scaleX: 1.55, scaleY: 1.55 }, 500, Laya.Ease.circInOut)
+			Laya.Tween.to(this, { scaleX: 1.15, scaleY: 1.15 }, 500, Laya.Ease.circInOut)
 			Laya.timer.once(300, this, () => {
 				Laya.Tween.to(this, { rotateAngle: rotateAngle }, 500, Laya.Ease.circInOut)
 			})
@@ -37,8 +35,8 @@ module gamecomponent.object {
 			this.isUIShow = true;
 			Laya.Tween.clearAll(this);
 			Laya.Tween.clearAll(this.pos);
-			Laya.Tween.to(this, { scaleX: 1.55, scaleY: 1.55 }, 300, Laya.Ease.circInOut, Handler.create(this, () => {
-				Laya.Tween.to(this.pos, { x: this.targe_pos.x, y: this.targe_pos.y, scaleX: 1.4, scaleY: 1.4 }, time, Laya.Ease.backIn, Handler.create(this, () => {
+			Laya.Tween.to(this, { scaleX: 1.15, scaleY: 1.15 }, 300, Laya.Ease.circInOut, Handler.create(this, () => {
+				Laya.Tween.to(this.pos, { x: this.targe_pos.x, y: this.targe_pos.y, scaleX: 1, scaleY: 1 }, time, Laya.Ease.backIn, Handler.create(this, () => {
 					this.isFinalPos = true;
 					game.sceneObjectMgr.clearOfflineObject(this);
 				}));
